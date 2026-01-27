6a755dedec65baae86213efc1faad6b45a6b2f3f
Kamesh Tripathi <120265582+KameshTripathi@users.noreply.github.com>
2026-01-27 08:53:46 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (37.69%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
@@ -1,17 +1,38 @@
+// class Solution {
+// public:
+//     int maxNumberOfBalloons(string text) {
+//         string s = "balloon";
+//         vector<int> mp(26, 0);
+//         for (auto ch : text) mp[ch - 'a']++;
+//         int count = 0;
+//         while (true) {
+//             for (auto ch : s) {
+//                 mp[ch - 'a']--;
+//                 if (mp[ch - 'a'] < 0) return count;
+//             }
+//             count++;
+//         }
+//         return 0;
+//     }
+// };
+
+
 class Solution {
 public:
     int maxNumberOfBalloons(string text) {
         string s = "balloon";
-        vector<int> mp(26, 0);
-        for (auto ch : text) mp[ch - 'a']++;
-        int count = 0;
-        while (true) {
-            for (auto ch : s) {
-                mp[ch - 'a']--;
-                if (mp[ch - 'a'] < 0) return count;
-            }
-            count++;
+        unordered_map<char, int> textMp;
+        unordered_map<char, int> sMp;
+        for (auto ch : text) textMp[ch]++;
+        for (auto ch : s) sMp[ch]++;
+        int mini = INT_MAX;
+        for (auto pair : sMp) {
+            int key = pair.first;
+            int need = pair.second;
+            int have = textMp[key];
+            int times = have / need;
+            mini = min(mini, times);
         }
-        return 0;
+        return mini;
     }
 };
